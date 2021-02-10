0fcdf54b907eb9349d7f7543b9b0ebc9d7e4bc58
贻伟 张 <137208403@qq.com>
2021-02-10 15:30:40 +0800

    28.实现strStr

---
+++ 28.实现strStr/my_solution_kmp.cpp
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode.cn id=28 lang=cpp
+ *
+ * [28] 实现 strStr()
+ */
+
+// @lc code=start
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    void next_val(string needle, vector<int> &next)
+    {
+        if (!next.empty())
+            next.clear();
+
+        next.resize(needle.size() + 1);
+
+        next[0] = -1;
+        next[1] = 0;
+
+        int i = 1, j = 0;
+        while (i < needle.size())
+        {
+            if (j == -1 || needle[i] == needle[j])
+            {
+                i++;
+                j++;
+                next[i] = j;
+            }
+            else
+            {
+                j = next[j];
+            }
+        }
+    }
+
+    int strStr(string haystack, string needle)
+    {
+        if (needle.empty())
+            return 0;
+        if (haystack.size() < needle.size())
+            return -1;
+
+        vector<int> next;
+        next_val(needle, next);
+
+        //size_type 至 int 类型间转换，否则与 -1 比较时出错
+        int size1 = haystack.size();
+        int size2 = needle.size();
+
+        int i = 0, j = 0;
+        while (i < size1 && j < size2)
+        {
+            if (j == -1 || haystack[i] == needle[j])
+            {
+                i++;
+                j++;
+            }
+            else
+            {
+                j = next[j];
+            }
+        }
+
+        if (j == needle.size())
+            return i - j;
+        else
+            return -1;
+    }
+};
+// @lc code=end
