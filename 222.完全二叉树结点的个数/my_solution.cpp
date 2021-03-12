7e028c266201a0daf7112b0d45304e6c0ad3cba0
贻伟 张 <137208403@qq.com>
2021-03-12 20:30:29 +0800

    222.完全二叉树结点的个数

---
+++ 222.完全二叉树结点的个数/my_solution.cpp
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=222 lang=cpp
+ *
+ * [222] 完全二叉树的节点个数
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    int countNodes(TreeNode *root)
+    {
+        int count = 0;
+        Count(root, count);
+        return count;
+    }
+
+    void Count(TreeNode *root, int &count)
+    {
+        if (root == nullptr)
+            return;
+
+        count++;
+        Count(root->left, count);
+        Count(root->right, count);
+    }
+};
+// @lc code=end
